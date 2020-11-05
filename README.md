b6bd8fab01fb749c568acf8f2ba0162866e9b2f4
TreeGee73 <TreeGee73@gmail.com>
2020-11-04 22:14:58 -0700

    added to the script.js, updated README.md

--- README.md
+++ README.md
@@ -5,15 +5,15 @@ The purpose of this project is to create an application that an employee can use
 This project utilizes HTML, CSS, and JavaScript.
 
 GIVEN I need a new, secure password
-[x] I click the button to generate a password
+[ ] I click the button to generate a password
 [x] I am presented with a series of prompts for password criteria
 [x] prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
+[x] I select which criteria to include in the password
+[x] prompted for the length of the password
+[x] I choose a length of at least 8 characters and no more than 128 characters
+[x] prompted for character types to include in the password
+[x] I choose lowercase, uppercase, numeric, and/or special characters
+[x] I answer each prompt
 THEN my input should be validated and at least one character type should be selected
 WHEN all prompts are answered
 THEN a password is generated that matches the selected criteria
--- script.js
+++ script.js
@@ -1,46 +1,94 @@
+var smLttrs = "abcdefghijklmnopqrstuvwxyz";
+var capLttrs = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
+var nbrs = "1234567890";
+var spChars = "~!@#$%^&*()_+{}:?><;.,";
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// var pwButton = document.getElementById("generate")
-// var pwLowLets = confirm("Do you want your password to contain lower case letters?")
-// console.log(pwLowLets)
-// var pwCapLets = confirm("Do you want your password to contain capital letters?")
-// console.log(pwCapLets)
-// var pwNumbers = confirm("Do you want your password to contain numbers?")
-// console.log(pwNumbers)
-// var pwSpecChars = confirm("Do you want your password to contain special characters?")
-// console.log(pwSpecChars)
+// Determine the desired length of the password.
+var pwLength = prompt("Please indicate a password length between 8 and 128.")
+// Test if it's working correctly
+console.log(pwLength)
+if (pwLength >= 8 && pwLength <= 128) {
+  console.log(pwLength);
+}
+
+else {
+  prompt("Your password must be at least 8, but no more than 128, characters \nPlease enter a valid number between 8 & 128.")
+}
+
+// Determine if user wants lower case letters in the password.
+var qrySmLttrs = confirm("Do you want to use lower case (a-z) letters in your password?")
+// Test if it's working correctly
+console.log(qrySmLttrs)
+
+// Determine if user wants upper case letters in the password.
+var qryCapLttrs = confirm("Do you want to use upper case (A-Z) letters in your password?")
+// Test if it's working correctly
+console.log(qryCapLttrs)
+
+// Determine if user wants numbers in the password.
+var qryNbrs = confirm("Do you want to use numbers (0-9) in your password?")
+// Test if it's working correctly
+console.log(qryNbrs)
+
+// Determine if user wants special characters in the password.
+var qrySpChars = confirm("Do you want to use special characters (!#$%&'()*+,-./:;<=>?@[]^_`{|}~) letters in your password?")
+// Test if it's working correctly
+console.log(qrySpChars)
+
+// Validate at least one criteria was selected
 
 
 
 
+// Execute random character selection based on password length
+  for (var i = 0; i < pwLength; i++) {
 
+      // If lower case letters are wanted, randomly select a letter from the smLttrs array.
+      if (qrySmLttrs) {
+        var pwSmLttrs = smLttrs[Math.floor(Math.random () * smLttrs.length)];
+        // Test if it's working correctly
+        console.log(pwSmLttrs);
+      }
 
+      // If upper case letters are wanted, randomly select a letter from the capLttrs array.
+      else if (qryCapLttrs) {
+        var pwCapLttrs = capLttrs[Math.floor(Math.random () * capLttrs.length)];
+        // Test if it's working correctly
+        console.log(pwCapLttrs);
+      }
 
+      // If numbers are wanted, randomly select a number from the nbrs array.
+      else if (qryNbrs) {
+        var pwNbrs = nbrs[Math.floor(Math.random () * nbrs.length)];
+        // Test if it's working correctly
+        console.log(pwNbrs);
+      }
 
-
-
-
-
-
+      // If special characters are wanted, randomly select a character from the spChars array.
+      else if (qrySpChars) {
+        var pwSpChars = spChars[Math.floor(Math.random () * spChars.length)];
+        // Test if it's working correctly
+        console.log(pwSpChars);
+      }
+      
+      else {
+        alert("Please select at least one criteria!");
+        break;
+      }
+  }
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); {
-  var pwLowLets = confirm("Do you want your password to contain lower case letters?")
-  console.log(pwLowLets)
-  var pwCapLets = confirm("Do you want your password to contain capital letters?")
-  console.log(pwCapLets)
-  var pwNumbers = confirm("Do you want your password to contain numbers?")
-  console.log(pwNumbers)
-  var pwSpecChars = confirm("Do you want your password to contain special characters?")
-  console.log(pwSpecChars)
-}
+// generateBtn.addEventListener("click", writePassword);
