57cb03e42c03d668ffcc3bfc0b4661a3420f84c1
TreeGee73 <TreeGee73@gmail.com>
2020-11-06 08:34:53 -0700

    script.js update to fix output

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-var smLttrs = "abcdefghijklmnopqrstuvwxyz";
-var capLttrs = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
-var nbrs = "1234567890";
-var spChars = "~!@#$%^&*()_+{}:?><;.,";
-
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-// Determine the desired length of the password.
+
+function generatePassword(){
+  var smLttrs = "abcdefghijklmnopqrstuvwxyz";
+  var capLttrs = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
+  var nbrs = "1234567890";
+  var spChars = "~!@#$%^&*()_+{}:?><;.,";
+  var password = "";
+  var avalChars = "";
+
+  // Determine the desired length of the password.
 var pwLength = prompt("Please indicate a password length between 8 and 128.")
 // Test if it's working correctly
 console.log(pwLength)
 if (pwLength >= 8 && pwLength <= 128) {
-  console.log(pwLength);
+  console.log(pwLength)
 }
 
 else {
-  prompt("Your password must be at least 8, but no more than 128, characters \nPlease enter a valid number between 8 & 128.")
+  alert("Your password must be at least 8, but no more than 128, characters \nPlease enter a valid number between 8 & 128.")
+  return "";
 }
 
 // Determine if user wants lower case letters in the password.
@@ -39,56 +45,48 @@ var qrySpChars = confirm("Do you want to use special characters (!#$%&'()*+,-./:
 console.log(qrySpChars)
 
 // Validate at least one criteria was selected
-
-
-
-
 // Execute random character selection based on password length
-  for (var i = 0; i < pwLength; i++) {
+// If lower case letters are wanted, randomly select a letter from the smLttrs array.
+if (qrySmLttrs) {
+  avalChars += smLttrs
+}
 
-      // If lower case letters are wanted, randomly select a letter from the smLttrs array.
-      if (qrySmLttrs) {
-        var pwSmLttrs = smLttrs[Math.floor(Math.random () * smLttrs.length)];
-        // Test if it's working correctly
-        console.log(pwSmLttrs);
-      }
+// If upper case letters are wanted, randomly select a letter from the capLttrs array.
+if (qryCapLttrs) {
+  avalChars += capLttrs
+}
 
-      // If upper case letters are wanted, randomly select a letter from the capLttrs array.
-      else if (qryCapLttrs) {
-        var pwCapLttrs = capLttrs[Math.floor(Math.random () * capLttrs.length)];
-        // Test if it's working correctly
-        console.log(pwCapLttrs);
-      }
+// If numbers are wanted, randomly select a number from the nbrs array.
+if (qryNbrs) {
+  avalChars += nbrs
+}
 
-      // If numbers are wanted, randomly select a number from the nbrs array.
-      else if (qryNbrs) {
-        var pwNbrs = nbrs[Math.floor(Math.random () * nbrs.length)];
-        // Test if it's working correctly
-        console.log(pwNbrs);
-      }
+// If special characters are wanted, randomly select a character from the spChars array.
+if (qrySpChars) {
+  avalChars += spChars
+}
 
-      // If special characters are wanted, randomly select a character from the spChars array.
-      else if (qrySpChars) {
-        var pwSpChars = spChars[Math.floor(Math.random () * spChars.length)];
-        // Test if it's working correctly
-        console.log(pwSpChars);
-      }
-      
-      else {
-        alert("Please select at least one criteria!");
-        break;
-      }
-  }
+if (
+  !qrySmLttrs && !qryCapLttrs && !qryNbrs && !qrySpChars
+) {
+  alert("Please select at least one criteria!");
+  return "";
+}
+
+for (let i = 0; i < pwLength; i++) {
+  
+  password = password+avalChars[Math.floor(Math.random () * avalChars.length)];
+}
+return password;
+}
+
 
 // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-
-// }
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
