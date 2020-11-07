41cbbfb31bdc2cb65298a47758e7553881b42aa8
TreeGee73 <TreeGee73@gmail.com>
2020-11-06 17:47:06 -0700

    Final pass on script.js file, includes comments

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,43 @@
 // Assignment Code
 
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector('#generate');
 
 
 function generatePassword(){
-  var smLttrs = "abcdefghijklmnopqrstuvwxyz";
-  var capLttrs = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
-  var nbrs = "1234567890";
-  var spChars = "~!@#$%^&*()_+{}:?><;.,";
-  var password = "";
-  var avalChars = "";
+  let smLttrs = 'abcdefghijklmnopqrstuvwxyz';
+  let capLttrs = 'ABCDEFGHIJKLMNOPRSTUVWXYZ';
+  let nbrs = '1234567890';
+  let spChars = '~!@#$%^&*()_+{}:?><;.,';
+  let password = '';
+  let avalChars = '';
 
-  // Determine the desired length of the password.
-var pwLength = prompt("Please indicate a password length between 8 and 128.")
-// Test if it's working correctly
-console.log(pwLength)
+// Determine the desired length of the password.
+let pwLength = prompt('Please indicate a password length between 8 and 128.')
+
+// Verify the password length entered meets the length requirements
 if (pwLength >= 8 && pwLength <= 128) {
   console.log(pwLength)
 }
 
+// If password length provided is less than the min or more than the max
+// will return and error and require the user to start again.
+// Password length criteria must be met before user can select desired password character content.
 else {
-  alert("Your password must be at least 8, but no more than 128, characters \nPlease enter a valid number between 8 & 128.")
-  return "";
+  alert('Your password must be at least 8, but no more than 128, characters \nPlease enter a valid number between 8 & 128.')
+  return '';
 }
 
 // Determine if user wants lower case letters in the password.
-var qrySmLttrs = confirm("Do you want to use lower case (a-z) letters in your password?")
-// Test if it's working correctly
-console.log(qrySmLttrs)
+let qrySmLttrs = confirm('Do you want to use lower case (a-z) letters in your password?')
 
 // Determine if user wants upper case letters in the password.
-var qryCapLttrs = confirm("Do you want to use upper case (A-Z) letters in your password?")
-// Test if it's working correctly
-console.log(qryCapLttrs)
+let qryCapLttrs = confirm('Do you want to use upper case (A-Z) letters in your password?')
 
 // Determine if user wants numbers in the password.
-var qryNbrs = confirm("Do you want to use numbers (0-9) in your password?")
-// Test if it's working correctly
-console.log(qryNbrs)
+let qryNbrs = confirm('Do you want to use numbers (0-9) in your password?')
 
 // Determine if user wants special characters in the password.
-var qrySpChars = confirm("Do you want to use special characters (!#$%&'()*+,-./:;<=>?@[]^_`{|}~) letters in your password?")
-// Test if it's working correctly
-console.log(qrySpChars)
+let qrySpChars = confirm('Do you want to use special characters (!#$%&\'()*+,-./:;<=>?@[]^_`{|}~) letters in your password?')
 
 // Validate at least one criteria was selected
 // Execute random character selection based on password length
@@ -65,17 +60,20 @@ if (qryNbrs) {
 if (qrySpChars) {
   avalChars += spChars
 }
-
+// Will stop the process and return an alert if there are no character types selected.
 if (
   !qrySmLttrs && !qryCapLttrs && !qryNbrs && !qrySpChars
 ) {
-  alert("Please select at least one criteria!");
-  return "";
+  return alert('Please select at least one criteria!');
+  
 }
 
 for (let i = 0; i < pwLength; i++) {
-  
-  password = password+avalChars[Math.floor(Math.random () * avalChars.length)];
+  // Selects the characters that will be in the password randomly from
+  // the characters that were pulled into availChars. Will loop through
+  // selecting characters at random until there are enough characters to
+  // fullfil the password length requirement
+  password +=avalChars[Math.floor(Math.random () * avalChars.length)];
 }
 return password;
 }
@@ -83,10 +81,10 @@ return password;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector('#password');
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
